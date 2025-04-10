9fbae11f1673a07be373b06a0601f0c1d3859142
KhaledMahm0vd <12422020643438@pg.cu.edu.eg>
2025-04-10 12:30:38 +0200

    004-005-006

---
+++ 1.intro_cs/000.python_for_everybody/code/0-if.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+n = 5
+while n > 0:
+	print(n)
+	n = n - 1
+print("blastoff")
---
+++ 1.intro_cs/000.python_for_everybody/code/1-ifelse.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+x = 5
+if x < 10:
+    print('Smaller')
+elif x > 20:
+    print("bigger")
+else:
+    print("Mid")
+print("Finish")
